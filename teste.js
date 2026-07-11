fd0313f8b4dc4652f9b7d1bed5bec1d987692153
Samuel Ferreira Guimarães <samuelguimaa@gmail.com>
2026-07-11 11:56:45 -0300

    extrai atualizacao do concurso PF com cheerio

--- teste.js
+++ teste.js
@@ -1,8 +1,11 @@
 const axios = require('axios');
+const cheerio = require('cheerio');
 
 async function testar() {
-  const resposta = await axios.get('https://cebraspe.org.br');
-  console.log(resposta.data);
+  const resposta = await axios.get('https://www.estrategiaconcursos.com.br/blog/concurso-pf-ultimas-noticias/');
+  const $ = cheerio.load(resposta.data);
+  const atualizacao = $('h2:contains("situação do Concurso PF")').next('p').text().trim().replace(/\s+/g, ' ');
+  console.log(atualizacao);
 }
 
 testar();
