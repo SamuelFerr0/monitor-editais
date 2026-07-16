5636b4a9f40e630e9e0291f3859581f459d1b23c
Samuel Ferreira Guimarães <samuelguimaa@gmail.com>
2026-07-16 11:43:12 -0300

    alteração no scraper

--- scraper.js
+++ scraper.js
@@ -1,8 +1,11 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const url = 'https://www.estrategiaconcursos.com.br/blog/concurso-pf-ultimas-noticias/'
 
 async function buscarAtualizacaoPF() {
-  const resposta = await axios.get('https://www.estrategiaconcursos.com.br/blog/concurso-pf-ultimas-noticias/');
+  const resposta = await axios.get(url, {
+  headers: { 'User-Agent': 'Mozilla/5.0' },
+  timeout: 10000 });
   const $ = cheerio.load(resposta.data);
   const atualizacao = $('h2:contains("situação do Concurso PF")').next('p').text().trim().replace(/\s+/g, ' ');
   return atualizacao;
